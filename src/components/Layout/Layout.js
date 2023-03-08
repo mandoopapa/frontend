e4b8db0222fb222bdf69be80e6b71c6da7cc3912
DESKTOP-L2IUB23\이재성 <remonytea@gmail.com>
2023-03-08 15:28:56 +0900

    update 0308

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,6 +10,7 @@ const Layout = () => {
   return (
     <>
       <Header />
+			
 			<div className='body'>
 				<Introduction />
 
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,16 +2,15 @@
 import './Layout/layout.css'
 import { Transfer } from './Transfer'
 
-const News = () => {
+function News() {
   return (
 	<div className='news'>
 		<div>
 			<span className='report_title'>Max Lee</span>
 			<p>Latest News</p>
-			<a href='https://www.bonmedia.kr/news/articleView.html?idxno=456' target='_blank' rel='noreferrer'>레이어2 ‘시바리움’ 기대감에 ‘시바이누’ 소각률 1700% ↑</a>
+			<a href='https://www.bonmedia.kr/news/articleView.html?idxno=456' target='_blank' rel='noreferrer'>칠리즈, 14일 칠리즈 토큰 홀더 대상 밋업 개최</a>
 			<p />
-			<a href='https://www.bonmedia.kr/news/articleView.html?idxno=430' target='_blank' rel='noreferrer'>日 게이오 전철, 1020세대 전철 이용 활성화 위해 NFT 발행</a>
-			
+			<a href='https://www.bonmedia.kr/news/articleView.html?idxno=430' target='_blank' rel='noreferrer'>유가랩스, 비트코인 NFT 트웰브폴드 시리즈 완판</a>
 			<p />
 			<a href="https://www.bonmedia.kr/news/articleList.html?sc_area=A&view_type=sm&sc_word=%EC%9D%B4%EC%9E%AC%EC%84%B1" target='_blank' rel='noreferrer'>See the latest News on BonMedia</a>
 			<p />
@@ -26,7 +25,7 @@ const News = () => {
 		</div>
 
 		<div>
-			<h2>Report Name #1</h2>
+			<h2>...</h2>
 			<p>Latest News 1</p>
 			<p>Latest News 2</p>
 			<a href="/#">See the latest News on BonMedia</a>
